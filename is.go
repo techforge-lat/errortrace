6f097b95459f3cb059ac701fc8bf2bf0d16b9fe0
Hernan Reyes <hernan_rm@outlook.es>
2023-11-28 06:22:42 -0600

    fix: Is validation when *Error

--- is.go
+++ is.go
@@ -5,7 +5,7 @@ import "errors"
 func Is(err error, target error) bool {
 	var errTrace *Error
 	if errors.As(err, &errTrace) {
-		return errors.Is(err, errTrace.err)
+		return errors.Is(errTrace.err, target)
 	}
 
 	return errors.Is(err, target)
