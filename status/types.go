9171f9e6263f023fda87886dee9b2af50ac6706d
Hernan Reyes <hernan_rm@outlook.es>
2024-03-01 07:35:16 -0600

    feat: adds status types and refactor empty validations

--- status/types.go
+++ status/types.go
@@ -5,19 +5,21 @@ import "net/http"
 type Code string
 
 const (
-	BadRequest    Code = "bad_request"
-	InternalError Code = "internal_error"
-	BindFailed    Code = "bind_failed"
-	Unauthorized  Code = "unauthorized"
-	Forbidden     Code = "forbidden"
-	NotFound      Code = "not_found"
+	BadRequest          Code = "bad_request"
+	UnprocessableEntity Code = "unprocessable_entity"
+	InternalError       Code = "internal_error"
+	BindFailed          Code = "bind_failed"
+	Unauthorized        Code = "unauthorized"
+	Forbidden           Code = "forbidden"
+	NotFound            Code = "not_found"
 )
 
 var HTTPStatusByStatusCode = map[Code]int{
-	BadRequest:    http.StatusBadRequest,
-	InternalError: http.StatusInternalServerError,
-	BindFailed:    http.StatusBadRequest,
-	Unauthorized:  http.StatusUnauthorized,
-	Forbidden:     http.StatusForbidden,
-	NotFound:      http.StatusNotFound,
+	BadRequest:          http.StatusBadRequest,
+	UnprocessableEntity: http.StatusUnprocessableEntity,
+	InternalError:       http.StatusInternalServerError,
+	BindFailed:          http.StatusBadRequest,
+	Unauthorized:        http.StatusUnauthorized,
+	Forbidden:           http.StatusForbidden,
+	NotFound:            http.StatusNotFound,
 }
